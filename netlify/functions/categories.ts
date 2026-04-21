2c8abeaaf633f51cbf39d1bcaafec97cc479aedc
Moon Tech <moontechknowledge@gmail.com>
2026-04-22 03:11:28 +0530

    add categories function

---
+++ netlify/functions/categories.ts
@@ -0,0 +1,10 @@
+export const handler = async () => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify([
+      { id: 1, name: "Restaurants" },
+      { id: 2, name: "Electronics" },
+      { id: 3, name: "Groceries" }
+    ]),
+  };
+};
